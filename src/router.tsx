53e9604aa92c4c96160651324818dd60ee340f58
강지원 <kkgi1234@gmail.com>
2026-05-06 13:51:07 +0900

    feature: 튜토리얼 리스트와 튜토리얼 각 단원 진입 페이지를 연결하는 라우트 추가

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { HomePage } from './pages/HomePage';
 import { TutorialsPage } from './pages/TutorialsPage';
+import { TutorialDetailPage } from './pages/TutorialDetailPage';
 import { PlaygroundPage } from './pages/PlaygroundPage';
 import { NotFoundPage } from './pages/NotFoundPage';
 
@@ -12,6 +13,7 @@ export const router = createBrowserRouter([
     children: [
       { index: true, element: <HomePage /> },
       { path: 'tutorials', element: <TutorialsPage /> },
+      { path: 'tutorials/:slug', element: <TutorialDetailPage /> },
       { path: 'playground', element: <PlaygroundPage /> },
       { path: '*', element: <NotFoundPage /> },
     ],
